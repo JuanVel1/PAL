12fcaf148616926780861dbba83b9f014d794d2a
Julian Rivera <jrivera@consensussa.com>
2025-05-14 22:03:24 -0500

    Merge

--- src/main/java/com/example/pal/repository/ContentRepository.java
+++ src/main/java/com/example/pal/repository/ContentRepository.java
@@ -2,10 +2,11 @@ package com.example.pal.repository;
 
 import com.example.pal.model.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
